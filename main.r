7fa4bd04ccaf5d994c8fdfe2b4e3a828198a2a31
Freddy Drennan <drennanfreddy@gmail.com>
2022-09-02 01:00:31 -0600

    save

--- main.r
+++ main.r
@@ -28,7 +28,7 @@ ui <- function() {
          ),
          shiny$tabPanel(
            'Preview Data',
-           shiny$tableOutput('previewData')
+           shiny$uiOutput('previewData')
          )
        )
      ))
@@ -36,7 +36,8 @@ ui <- function() {
 }
 
 server <- function(input, output, session) {
-   box::use(./box/ui/meta, shiny, dplyr, fs, cli, glue, purrr, readr, haven)
+   box::use(./box/ui/meta, shiny, dplyr, fs, cli, glue, purrr, readr, haven,
+            htmlTable)
    filteredData <- meta$server_metadata()
    
    shiny$observeEvent(input$deleteDevelopmentFolder, {
@@ -82,12 +83,13 @@ server <- function(input, output, session) {
    
    dataToAnalyze <- reactive({
      shiny$req(filteredData())
-     data <- filteredData()
+     data <- filteredData() |> 
+       dplyr$arrange(dplyr$desc(date))
      if (getOption('development')) {
        data$path <- paste0(getOption('datamisc_cache_path'), data$path)
      } 
      n_files <- nrow(data)
-     data <- purrr$imap_dfr(
+     data <- purrr$imap(
        split(data, 1:n_files),
        function(data, y) {
          y <- as.numeric(y)
@@ -125,12 +127,68 @@ server <- function(input, output, session) {
      data
    })
    
-   output$previewData <- shiny$renderTable({
+   output$previewData <- shiny$renderUI({
      shiny$req(dataToAnalyze)
      data <- dataToAnalyze()
+     dataNames <- purrr$map(data, function(x) {
+       list(
+         study = unique(x$study),
+         date = unique(x$date),
+         analysis = unique(x$analysis),
+         column_names = colnames(x),
+         data = x
+       )
+     })
      
-     data |> 
-       head(100)
+     purrr$map(
+       dataNames,
+       function(x) {
+         data <- x$data
+         nrowData <- nrow(data)
+         column_names <- x$column_names
+         column_names <- column_names[column_names==toupper(column_names)]
+         nMissing <- data |> 
+           dplyr$summarise_all(function(x) {
+             sum(is.na(x))/nrowData             
+           })
+         
+         nMissing <- 
+           nMissing |> 
+           dplyr$select_if(
+            function(x) {
+             x > 0
+           } 
+         )
+         
+         if (ncol(nMissing)) {
+           nMissing <- nMissing |> 
+             dplyr$mutate_all(function(x) {
+               paste0(
+                 round(x, 2), "%"
+               )
+             })
+           colnames(nMissing) <- paste0(' - ', colnames(nMissing), ' - ')
+           nMissing <- shiny$div(
+             shiny$h3('Missing Data'),
+             htmlTable$htmlTable(nMissing) 
+           )
+             
+         } else {
+           nMissing <- NULL
+         }
+         # browser()
+         shiny$wellPanel(
+           shiny$h3(glue$glue('Study: {x$study}')),
+           shiny$h3(glue$glue('Date: {x$date}')),
+           shiny$h3(glue$glue('Analysis: {x$analysis}')),
+           shiny$h3('Columns'),
+           purrr$map(column_names, function(x) {
+             shiny$tags$em(paste0(x, " * "))
+           }),
+           nMissing
+         )
+       }
+     )
       
    })
 }
