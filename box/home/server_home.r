ea54fb1a2c7e8242275a03c3712fdcc49e39a451
Freddy <drennanfreddy@gmail.com>
2022-09-04 20:34:13 -0600

    save

--- box/home/server_home.r
+++ box/home/server_home.r
@@ -17,60 +17,62 @@ server <- function(input, output, session) {
   output$previewData <- shiny$renderUI({
     shiny$req(dataToAnalyze)
     dataNames <- dataToAnalyze()
-
     purrr$map(
       dataNames,
       function(x) {
         # For each analysis, grab the file
         data <- x$data
-        nrowData <- nrow(data)
-        
-        nMissing <- data |>
-          dplyr$summarise_all(function(x) {
-            sum(is.na(x)) / nrowData
-          })
-
-        nMissing <-
-          nMissing |>
-          dplyr$select_if(
-            function(x) {
-              x > 0
-            }
-          )
-        if (ncol(nMissing)) {
-          nMissing <- nMissing |>
-            dplyr$mutate_all(function(x) {
-              paste0(
-                round(x, 2), "%"
-              )
-            })
-          colnames(nMissing) <- paste0(" - ", colnames(nMissing), " - ")
-          nMissing <- shiny$div(
-            shiny$h4("Missing Data"),
-            shiny$div(
-              htmlTable$htmlTable(nMissing)
-            )
-          )
-        } else {
-          nMissing <- shiny$div()
-        }
-        if ("PARAMCD" %in% colnames(x$data)) {
-          paramcd <- unique(x$data[, "PARAMCD"]$PARAMCD)
-          paramcd <- shiny$div(
-            shiny$h3("PARAMCD"),
-            purrr$map(paramcd, function(x) {
-              shiny$tags$em(paste0(x, " * "))
-            })
-          )
-        } else {
-          paramcd <- shiny$div()
-        }
-        bs4Dash$bs4Card(width=12,
-          title = toupper(x$analysis), closable=TRUE,
-          paramcd,
-          nMissing
+        bs4Dash$bs4Card(
+          title = toupper(x$analysis), closable=TRUE
         )
       }
     )
   })
+}
+
+
+
+todo <- function() {
+  nrowData <- nrow(data)
+  
+  nMissing <- data |>
+    dplyr$summarise_all(function(x) {
+      sum(is.na(x)) / nrowData
+    })
+  
+  nMissing <-
+    nMissing |>
+    dplyr$select_if(
+      function(x) {
+        x > 0
+      }
+    )
+  if (ncol(nMissing)) {
+    nMissing <- nMissing |>
+      dplyr$mutate_all(function(x) {
+        paste0(
+          round(x, 2), "%"
+        )
+      })
+    colnames(nMissing) <- paste0(" - ", colnames(nMissing), " - ")
+    nMissing <- shiny$div(
+      shiny$h4("Missing Data"),
+      shiny$div(
+        htmlTable$htmlTable(nMissing)
+      )
+    )
+  } else {
+    nMissing <- shiny$div()
+  }
+  if ("PARAMCD" %in% colnames(x$data)) {
+    paramcd <- unique(x$data[, "PARAMCD"]$PARAMCD)
+    paramcd <- shiny$div(
+      shiny$h3("PARAMCD"),
+      purrr$map(paramcd, function(x) {
+        shiny$tags$em(paste0(x, " * "))
+      })
+    )
+  } else {
+    paramcd <- shiny$div()
+  }
 }
