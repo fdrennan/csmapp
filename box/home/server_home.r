5f38501a2a650a2cfd772bee829d3099397c1cc4
Freddy <drennanfreddy@gmail.com>
2022-09-04 22:18:16 -0600

    save

--- box/home/server_home.r
+++ box/home/server_home.r
@@ -18,49 +18,74 @@ server <- function(input, output, session) {
   output$previewData <- shiny$renderUI({
     shiny$req(dataToAnalyze)
     data <- dataToAnalyze()
-    data <- purrr$keep(data, function(x) {x$analysis=='aei'})
+
     purrr$map(
       data,
       function(x) {
         data <- x$data
-        bs4Dash$bs4Card(width = 12,
-                        id=paste0('analysisSetup',x$analysis),
+        bs4Dash$bs4Card(
+          width = 12,
+          id = paste0("analysisSetup", x$analysis),
           title = toupper(x$analysis),
           closable = TRUE,
+          sidebar = bs4Dash$cardSidebar(
+            id = paste0("analysisSetup", x$analysis, "sidebar"),
+            startOpen = TRUE,
+            easyClose = TRUE,
+            shiny$numericInput(paste0(x$analysis, "statsSplit"), "Stats Split",
+              value = 1,
+              min = 1, max = 10, step = 1
+            )
+          ),
+          shiny$uiOutput(paste0("statsSplitUI", x$analysis))
+        )
+      }
+    )
+  })
+
+
+  output$statsSplitUIaei <- shiny$renderUI({
+    shiny$req(input$aeistatsSplit)
+    data <- dataToAnalyze()
+    data <- purrr$keep(data, function(x) {
+      x$analysis %in% c("aei", "rgv")
+    })
+    purrr$map(
+      1:input$aeistatsSplit,
+      function(x) {
+        bs4Dash$box(
+          width = 12,
+          shiny$selectizeInput(paste0('aei', x, "statsGroupPARAMCD"),
+                               glue$glue("Stats Group {x}"),
+                               choices = data[[1]]$PARAMCD,
+                               selected = data[[1]]$PARAMCD, multiple = TRUE
+          ),
           shiny$fluidRow(
             shiny$column(
-              3,
-              shiny$numericInput(paste0(x$analysis, "statsSplit"), "Stats Split",
-                                 value = 1,
-                                 min = 1, max = 10, step = 1
-              ), 
-              bs4Dash$actionButton(paste0('submitStatsSplit', x$analysis), 'Go')
+              6,
+              shiny$numericInput(paste0('aei', x, "nStatistics"),
+                                 "n",
+                                 min = -Inf, max = Inf, value = 2
+              )
             ),
             shiny$column(
-              9, 
-              shiny$uiOutput(paste0('statsSplitUI', x$analysis))
+              6,
+              shiny$numericInput(paste0('aei', x, "rStatistics"),
+                                 "r",
+                                 min = -Inf, max = Inf, value = 2
+              )
+            ),
+            shiny$column(
+              6,
+              shiny$numericInput(paste0('aei', x, "diff_pctStatistics"),
+                                 "diff_pct",
+                                 min = -Inf, max = Inf, value = 10
+              )
             )
           )
         )
       }
     )
-  })
-  
-  
-  output$statsSplitUIaei <- shiny$renderUI({
-    shiny$req(input$submitStatsSplitaei)
-    shiny$req(input$aeistatsSplit)
-    data <- dataToAnalyze()
-    data <- purrr$keep(data, function(x) {x$analysis=='aei'})
-    purrr$map(
-      1:input$aeistatsSplit,
-      function(x) {
-        shiny$selectizeInput(paste0(x, 'statsGroupPARAMCD'),
-                             'Stats Group', choices = data[[1]]$PARAMCD,
-                             selected = data[[1]]$PARAMCD, multiple = TRUE)
-      }
-    )
-    # browser()
   })
 }
 
--- box/processing.r
+++ box/processing.r
@@ -9,36 +9,30 @@ make_data_to_analyze <- function(filteredData) {
   if (getOption("development")) {
     data$path <- paste0(getOption("datamisc_cache_path"), data$path)
   }
-  n_files <- nrow(data)
-  data <- purrr$imap(
-    split(data, 1:n_files),
-    function(data, y) {
-      y <- as.numeric(y)
-      with(
-        data,
-        {
-          path_ext_type <- fs$path_ext(path)
-          shiny$showNotification(shiny$div(
-            shiny$h6(glue$glue(
-              "Study {study}"
-            )),
-            shiny$h6(
-              glue$glue(
-                "Filename {filename}"
-              )
-            ),
-            shiny$h6(
-              glue$glue(
-                "Files Remaining: {n_files-y}"
-              )
-            )
-          ), duration = 2, closeButton = FALSE)
-          study_data <- switch(path_ext_type,
-            "csv" = readr$read_csv(path),
-            "sas7bdat" = haven$read_sas(path)
-          )
 
-          dplyr$bind_cols(data, study_data)
+
+  n_files <- nrow(data)
+  data <- shiny$withProgress(
+    message = "Reading In Data",
+    detail = "This may take a while...",
+    value = 0,
+    {
+      purrr$imap(
+        split(data, 1:n_files),
+        function(data, y) {
+          y <- as.numeric(y)
+          with(
+            data,
+            {
+              path_ext_type <- fs$path_ext(path)
+              study_data <- switch(path_ext_type,
+                "csv" = readr$read_csv(path),
+                "sas7bdat" = haven$read_sas(path)
+              )
+              shiny$incProgress(1 / n_files)
+              dplyr$bind_cols(data, study_data)
+            }
+          )
         }
       )
     }
