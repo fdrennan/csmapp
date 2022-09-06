6e8b5c3efda2c4bec5a0667d2a11313a1930f33f
Freddy <drennanfreddy@gmail.com>
2022-09-06 10:36:46 -0600

    save

--- box/lm/server_lm.R
+++ box/lm/server_lm.R
@@ -116,7 +116,7 @@ server <- function(id, parentSession, inputData) {
       })
 
       output$flaggingTemplate <- shiny$renderUI({
-        shiny$req(input$n, input$r, input$diff_pct)
+        shiny$req(flaggingFilter())
         shiny$div(
           shinyAce$aceEditor(
             outputId = ns("flagInput"), value = flaggingFilter()$flag_crit, theme = "chaos",
@@ -127,8 +127,7 @@ server <- function(id, parentSession, inputData) {
       })
 
       gluedFlagData <- shiny$reactive({
-        input$update
-        shiny$isolate(input)
+        shiny$req(input$flagInput)
         flagInput <- with(
           shiny$reactiveValuesToList(input),
           glue$glue(input$flagInput)
