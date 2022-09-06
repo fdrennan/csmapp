99c6bdfb386b98415730a2bba8faefe0658db559
Freddy <drennanfreddy@gmail.com>
2022-09-06 00:58:52 -0600

    sav

--- box/lm/server_lm.R
+++ box/lm/server_lm.R
@@ -10,8 +10,8 @@ server <- function(id, parentSession, inputData) {
     )
   }
 
-
   box::use(shiny, cli, bs4Dash, glue, shinyAce, stringr, styler)
+
   shiny$moduleServer(
     id,
     function(input, output, session) {
@@ -45,24 +45,35 @@ server <- function(id, parentSession, inputData) {
             shiny$column(
               6,
               shiny$wellPanel(
-                shiny$selectInput(ns('flagValue'), 'Flag', choices = c(-1, 0, 1), selected = 1),
+                shiny$selectInput(ns("flagValue"), "Flag", choices = c(-1, 0, 1), selected = 1),
                 shiny$selectizeInput(ns("statsGroupPARAMCD"), "PARAMCD",
-                                     choices = PARAMCD,
-                                     selected = PARAMCD, multiple = TRUE
+                  choices = PARAMCD,
+                  selected = PARAMCD, multiple = TRUE
                 ),
                 shiny$tags$hr(),
-                shiny$textInput(ns("variableName"), label=NULL, placeholder = 'Variable Name'),
-                shiny$div(class = "text-right", 
-                          bs4Dash$actionButton(ns("addVariable"), 
-                                               shiny$tags$em("Add variable")))
+                shiny$textInput(ns("variableName"), label = NULL, placeholder = "Variable Name"),
+                shiny$div(
+                  class = "text-right",
+                  bs4Dash$actionButton(
+                    ns("addVariable"),
+                    shiny$tags$em("Add variable")
+                  )
+                )
               )
-            ), 
+            ),
             shiny$column(
               6, shiny$uiOutput(ns("statisticsSetup"))
+            ),
+            shiny$column(
+              6, shiny$uiOutput(ns("flaggingTemplate"))
+            ),
+            shiny$column(
+              6, shiny$uiOutput(ns("flaggingPreview"))
             )
           )
         )
       })
+
 
       output$statisticsSetup <- shiny$renderUI({
         shiny$req(inputData)
@@ -70,10 +81,8 @@ server <- function(id, parentSession, inputData) {
         PARAMCD <- data[[1]]$PARAMCD
         analysis <- data[[1]]$analysis
 
-        switch(
-          analysis,
-          'aei' = {
-            browser()
+        switch(analysis,
+          "aei" = {
             currentFlag <- input$flag
             shiny$wellPanel(
               shiny$numericInput(
@@ -86,7 +95,7 @@ server <- function(id, parentSession, inputData) {
               ),
               shiny$numericInput(
                 ns("diff_pctStatistics"), "diff_pct",
-                min = -Inf, max = Inf, value = 10
+                min = -Inf, max = Inf, value = -10
               ),
               shiny$div(id = ns("variables"))
             )
@@ -94,9 +103,22 @@ server <- function(id, parentSession, inputData) {
         )
       })
 
+      output$flaggingTemplate <- shiny$renderUI({
+        # shiny$req(input$textTemplate)
+        flag_crit <- "if ((abs(diff_pct)/100*n>{n} & diff_pct<{diff_pct}) & ( p_value<0.05 | r=={r})) {flag= -1};"
+        shinyAce$aceEditor(outputId = ns('flagInput'), value = flag_crit, theme = 'chaos',
+                           fontSize = 18, wordWrap = TRUE, mode = 'r', autoComplete = 'enabled')
+      })
+
+      output$flaggingPreview <- shiny$renderUI({
+        shiny$req(input$flagInput)
+        shiny$debounce(input$flagInput, 100)
+        shinyAce$aceEditor(outputId = ns('flag'), value = input$flagInput, theme = 'chaos')
+      })
+
       shiny$observeEvent(input$addVariable, {
-        if (input$variableName=='') {
-          shiny$showNotification('No variable name supplied', type ='warning')
+        if (input$variableName == "") {
+          shiny$showNotification("No variable name supplied", type = "warning")
         }
         shiny$req(input$variableName)
         shiny$insertUI(
@@ -106,7 +128,6 @@ server <- function(id, parentSession, inputData) {
           }
         )
       })
-      
     },
     session = parentSession
   )
